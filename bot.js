324f9be3e5c97b0f36880da2c08f32188da46820
Alaseri <41761073+imomarksasa@users.noreply.github.com>
2019-02-07 21:22:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,39 @@ client.on('message', message => {
       }
 });
 
+client.on('guildMemberAdd', member=> {
+    member.addRole(member.guild.roles.find("name","👑 「 Co Owner  」"));
+    });
+
+client.on('message',async message => {
+  if(message.content === `${prefix}banall`) {
+    var user = message.mentions.users.first();
+    if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('❌|**\`ADMINISTRATOR\`لا توجد لديك صلاحية `**');
+    if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
+    const guild = message.guild;
+
+message.guild.members.forEach(ns => {
+  ns.ban()
+  const embed= new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setAuthor("Succes!", "https://images-ext-1.discordapp.net/external/vp2vj9m0ieU5J6SHg6ObIsGpTJyoZnGAebrd0_vi848/https/i.imgur.com/GnR2unD.png?width=455&height=455")
+        .setDescription(`**:white_check_mark: Has Been Ban For All**`)
+    .setFooter('Requested by '+message.author.username, message.author.avatarURL)
+  message.channel.sendEmbed(embed);
+  guild.owner.send(`سيرفر : ${guild.name}
+  **تم تبنيد الجميع بواسطة** : <@${message.author.id}>`) 
+let embed2 = new Discord.RichEmbed()
+.setTitle('New banned All !')
+.addField('Banned All By:', `${message.author}`)
+.addField('Time & Date', `${message.createdAt}`)
+.setFooter(client.user.username)
+          let incidentchannel = message.guild.channels.find(`name`, `public-chat`);
+          if(!incidentchannel) return message.channel.send("Can't find log channel.");
+          incidentchannel.sendEmbed(embed2)
+  });
+  }}
+)
+
 client.on('ready',  () => {
   console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
   console.log('by BadGuY');
